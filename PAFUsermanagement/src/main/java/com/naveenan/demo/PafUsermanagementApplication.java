3ecad16015260cffaea3f42d8072c1e3dfec13e9
lahiruMendis <42890242+lahiruMendis@users.noreply.github.com>
2019-05-19 23:11:36 +0530

    File Upload
    new one

---
+++ PAFUsermanagement/src/main/java/com/naveenan/demo/PafUsermanagementApplication.java
@@ -0,0 +1,13 @@
+package com.naveenan.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PafUsermanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PafUsermanagementApplication.class, args);
+	}
+
+}
---
+++ PAFUsermanagement/src/main/java/com/naveenan/demo/model/User.java
@@ -0,0 +1,69 @@
+package com.naveenan.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "user")
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String username;
+    private String email; 
+    private String password;
+    
+	public User() {
+
+	}
+    
+    public User(long id, String username, String email, String password, String passwordConfirm) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.email = email;
+		this.password = password;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", email=" + email + ", password=" + password
+				+"]";
+	}
+	
+	
+	
+}
