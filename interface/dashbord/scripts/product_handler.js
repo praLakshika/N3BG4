c529c62aab04d96f401fd6c56dec2fd5a3b8b9df
janith29 <janithsandaruwan29@gmail.com>
2019-05-21 08:24:44 +0530

    product delete

--- interface/dashbord/scripts/product_handler.js
+++ interface/dashbord/scripts/product_handler.js
@@ -11,16 +11,18 @@ $("#products_productlist").ready(function () {
             <div class="personal-info-wrap">
                 <div class="widget-head-info-box">
                     <div class="persoanl-widget-hd">
-                        <h2 class="username_header_id" >${value.productName}</h2>
+                    <h2 class="username_header_id" >${value.productName}</h2>
+                    <h2 class="proId" >${value.productId}</h2>
                         <p>${value.creator}</p>
                     </div>
                     <img src="${value.productImage}" class="img-circle circle-border m-b-md center" alt="profile"  width="150" height="150">
+                    <input type="hidden" id="proId" name="proId" value="${value.productId}">
                     <div class="social-widget-result">
                         <span>${value.productPrice}</span> <br>
                         <span><button type="button" id="user_editUser_btn" class="btn btn-primary">
                         EDIT
                     </button></span> |
-                        <span> <button type="button" id="user_deleteUser_btn" class="btn btn-danger">
+                        <span> <button type="button" id="product_delete_btn" class="btn btn-danger">
                         DELETE
                     </button></span>
                        
@@ -33,5 +35,22 @@ $("#products_productlist").ready(function () {
         });
 
         $("#products_productlist").append(newItem);
+    });
+});
+$('body').on('click', '#product_delete_btn', function (event) {
+    var proId = $(event.target).parent().parent().parent().parent().find('.proId').html();
+
+       
+    $.ajax(`http://localhost:8080/sellnbye/api/product/${proId}`, {
+        
+        contentType: 'application/json',
+        type: 'DELETE'
+    }).done(function (response) {
+        location.reload();
+        if (response) {
+            alert("Successfully Deleted");
+        } else {
+            alert("Delete Failed");
+        }
     });
 });
